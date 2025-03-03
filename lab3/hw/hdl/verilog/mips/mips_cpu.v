d194bdbe7680ac21923b0ebed4a2801899ae5f44
noahand0 <70404752+noahand0@users.noreply.github.com>
2025-03-02 20:04:13 -0800

    Update mips_cpu.v, sh now working

--- lab3/hw/hdl/verilog/mips/mips_cpu.v
+++ lab3/hw/hdl/verilog/mips/mips_cpu.v
@@ -177,8 +177,8 @@ module mips_cpu (
 
     assign mem_write_en[3] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b00)) & (~mem_half_ex | (mem_addr[1:0] == 2'b00));
     assign mem_write_en[2] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b01)) & (~mem_half_ex | (mem_addr[1:0] == 2'b00));
-    assign mem_write_en[1] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b10)) & (~mem_half_ex | (mem_addr[1:0] == 2'b01));
-    assign mem_write_en[0] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b11)) & (~mem_half_ex | (mem_addr[1:0] == 2'b01));
+    assign mem_write_en[1] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b10)) & (~mem_half_ex | (mem_addr[1:0] == 2'b10));
+    assign mem_write_en[0] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b11)) & (~mem_half_ex | (mem_addr[1:0] == 2'b10));
 
     assign mem_addr = alu_result_ex;
    // assign mem_write_data = (mem_byte_ex) ? {4{mem_write_data_ex[7:0]}} : mem_write_data_ex;
@@ -196,7 +196,7 @@ module mips_cpu (
     // assign mem_out = (mem_byte_mem) ? mem_read_data_byte_extend : mem_read_data;
 
     assign mem_out = mem_byte_mem ? mem_read_data_byte_extend : 
-                (mem_half_mem ? mem_read_data_half_extend : mem_read_data);
+                    (mem_half_mem ? mem_read_data_half_extend : mem_read_data);
 
     assign reg_write_data_mem = mem_read_mem ? mem_out : alu_result_mem;
 
