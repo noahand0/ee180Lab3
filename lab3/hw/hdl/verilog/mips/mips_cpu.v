a534e5207b08cfe587a18ffaa785da704b17ff0c
Nicole <nmulvey12@gmail.com>
2025-03-04 18:02:24 -0800

    Update mips_cpu.v

--- lab3/hw/hdl/verilog/mips/mips_cpu.v
+++ lab3/hw/hdl/verilog/mips/mips_cpu.v
@@ -39,9 +39,11 @@ module mips_cpu (
     wire mem_we_id, mem_we_ex;
     wire mem_read_id, mem_read_ex, mem_read_mem;
     wire mem_byte_id, mem_byte_ex, mem_byte_mem;
+    wire mem_half_id, mem_half_ex, mem_half_mem; 
     wire mem_signextend_id, mem_signextend_ex, mem_signextend_mem;
     wire [7:0] mem_read_data_byte_select;
     wire [31:0] mem_read_data_byte_extend;
+    wire [31:0] mem_read_data_half_extend;
     wire mem_atomic_id, mem_atomic_ex, mem_atomic_en, mem_sc_mask_id;
     wire mem_sc_id, mem_sc_ex;
     wire alu_overflow;
@@ -93,6 +95,7 @@ module mips_cpu (
         .mem_write_data     (mem_write_data_id),
         .mem_read           (mem_read_id),
         .mem_byte           (mem_byte_id),
+        .mem_half           (mem_half_id),
         .mem_signextend     (mem_signextend_id),
         .reg_we             (reg_we_id),
         .movn               (movn_id),
@@ -120,7 +123,7 @@ module mips_cpu (
     );
 
     // Load-linked / Store-conditional
-    wire atomic_en = en & mem_read_id;
+    wire atomic_en = en; //& mem_read_id; // How to fix this? LW should impact atomic or not...
     dffarre       atomic  (.clk(clk), .ar(rst), .r(rst_id), .en(atomic_en), .d(mem_atomic_id), .q(mem_atomic_ex));
     dffarre       sc      (.clk(clk), .ar(rst), .r(rst_id), .en(en), .d(mem_sc_id), .q(mem_sc_ex));
 
@@ -136,6 +139,7 @@ module mips_cpu (
     dffarre mem_we_id2ex (.clk(clk), .ar(rst), .r(rst_id), .en(en), .d(mem_we_id & ~mem_sc_mask_id), .q(mem_we_ex));
     dffarre mem_read_id2ex (.clk(clk), .ar(rst), .r(rst_id), .en(en), .d(mem_read_id), .q(mem_read_ex));
     dffarre mem_byte_id2ex (.clk(clk), .ar(rst), .r(rst_id), .en(en), .d(mem_byte_id), .q(mem_byte_ex));
+    dffarre mem_half_id2ex (.clk(clk), .ar(rst), .r(rst_id), .en(en), .d(mem_half_id), .q(mem_half_ex)); //added by NN
     dffarre mem_signextend_id2ex (.clk(clk), .ar(rst), .r(rst_id), .en(en), .d(mem_signextend_id), .q(mem_signextend_ex));
 
     // needed for W stage
@@ -154,12 +158,13 @@ module mips_cpu (
         .alu_overflow   (alu_overflow) // maybe do something creative with this
     );
 
-    // needed for M stage
+    // needed for M stage 
     wire [31:0] sc_result = {{31{1'b0}},(mem_sc_ex & mem_we_ex)};
     wire [31:0] alu_sc_result_ex = mem_sc_ex ? sc_result : alu_result_ex;   // TODO: Need to conditionally inject SC value
     dffare #(32) alu_result_ex2mem (.clk(clk), .r(rst), .en(en), .d(alu_sc_result_ex), .q(alu_result_mem));
     dffare mem_read_ex2mem (.clk(clk), .r(rst), .en(en), .d(mem_read_ex), .q(mem_read_mem));
     dffare mem_byte_ex2mem (.clk(clk), .r(rst), .en(en), .d(mem_byte_ex), .q(mem_byte_mem));
+    dffare mem_half_ex2mem (.clk(clk), .r(rst), .en(en), .d(mem_half_ex), .q(mem_half_mem));//added by NN
     dffare mem_signextend_ex2mem (.clk(clk), .r(rst), .en(en), .d(mem_signextend_ex), .q(mem_signextend_mem));
 
     // needed for W stage
@@ -169,17 +174,30 @@ module mips_cpu (
     // assign mem_read_ex = 1'b0;
     // assign mem_read_mem = 1'b0;
     assign mem_read_en = mem_read_ex;
-    assign mem_write_en[3] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b00));
-    assign mem_write_en[2] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b01));
-    assign mem_write_en[1] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b10));
-    assign mem_write_en[0] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b11));
+
+    assign mem_write_en[3] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b00)) & (~mem_half_ex | (mem_addr[1:0] == 2'b00));
+    assign mem_write_en[2] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b01)) & (~mem_half_ex | (mem_addr[1:0] == 2'b00));
+    assign mem_write_en[1] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b10)) & (~mem_half_ex | (mem_addr[1:0] == 2'b10));
+    assign mem_write_en[0] = mem_we_ex & (~mem_byte_ex | (mem_addr[1:0] == 2'b11)) & (~mem_half_ex | (mem_addr[1:0] == 2'b10));
+
     assign mem_addr = alu_result_ex;
-    assign mem_write_data = (mem_byte_ex) ? {4{mem_write_data_ex[7:0]}} : mem_write_data_ex;
+   // assign mem_write_data = (mem_byte_ex) ? {4{mem_write_data_ex[7:0]}} : mem_write_data_ex;
+   assign mem_write_data = mem_byte_ex ? {4{mem_write_data_ex[7:0]}} : 
+                        (mem_half_ex ? {2{mem_write_data_ex[15:0]}} : mem_write_data_ex);
+
     assign mem_read_data_byte_select =  (alu_result_mem[1:0] == 2'b00) ? mem_read_data[31:24] :
                                        ((alu_result_mem[1:0] == 2'b01) ? mem_read_data[23:16] :
                                        ((alu_result_mem[1:0] == 2'b10) ? mem_read_data[15:8] : mem_read_data[7:0]));
     assign mem_read_data_byte_extend = {{24{mem_signextend_mem & mem_read_data_byte_select[7]}}, mem_read_data_byte_select};
-    assign mem_out = (mem_byte_mem) ? mem_read_data_byte_extend : mem_read_data;
+
+    wire [15:0] mem_read_data_half_select = (alu_result_mem[1:0] == 2'b0) ? mem_read_data[31:16] : mem_read_data[15:0];
+
+    assign mem_read_data_half_extend = {{16{mem_signextend_mem & mem_read_data_half_select[15]}}, mem_read_data_half_select};
+    // assign mem_out = (mem_byte_mem) ? mem_read_data_byte_extend : mem_read_data;
+
+    assign mem_out = mem_byte_mem ? mem_read_data_byte_extend : 
+                    (mem_half_mem ? mem_read_data_half_extend : mem_read_data);
+
     assign reg_write_data_mem = mem_read_mem ? mem_out : alu_result_mem;
 
     // needed for W stage
